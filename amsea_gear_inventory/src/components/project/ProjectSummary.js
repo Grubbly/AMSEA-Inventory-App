52d9abc91d20ec37efa773bb5d3874658346083b
orthogonaltrajectory@gmail.com <orthogonaltrajectory@gmail.com>
2018-12-22 18:50:31 -0900

    CSS background and form focus highlights

--- amsea_gear_inventory/src/components/project/ProjectSummary.js
+++ amsea_gear_inventory/src/components/project/ProjectSummary.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const ProjectSummary = () => {
     return(
-        <div className="card z-depth-0 project-summary">
+        <div className="card z-depth-0 teal lighten-2 project-summary">
             <div className="card-content grey-text text-darken-3">
                 <span className="card-title">Project Title</span>
                 <p> Posted by Grubsy </p>
-                <p className="grey-text"> 22nd December, 2:20am </p>
+                <p className="grey-text text-darken-3"> 22nd December, 2:20am </p>
             </div>
         </div>
     )
