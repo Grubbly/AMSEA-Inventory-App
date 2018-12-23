3994e37707a23d7d1f88cd69f80371de0eb60423
orthogonaltrajectory@gmail.com <orthogonaltrajectory@gmail.com>
2018-12-23 02:20:21 -0900

    Firebase document creation from frontend

--- amsea_gear_inventory/src/store/actions/projectActions.js
+++ amsea_gear_inventory/src/store/actions/projectActions.js
@@ -1,12 +1,19 @@
 // Deals with adding, deleting, etc. projects
 export const createProject = (project) => {
     return (dispatch, getState, {getFirebase, getFirestore}) => {
-        // Async calls to database here!
-        
-        // Carry on with dispatch as normal
-        dispatch({
-            type: 'CREATE_PROJECT',
-            project
-        })
+        // Async calls to database here!   
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Tristan',
+            authorLastName: 'Grubsy',
+            authorId: 7,
+            createdAt: new Date()
+        }).then(() => {
+            // Carry on with dispatch as normal
+            dispatch({type: 'CREATE_PROJECT', project});
+            }).catch((err) => {
+                dispatch({type: 'CREATE_PROJECT_ERROR', err});  
+            })
     }
 }
