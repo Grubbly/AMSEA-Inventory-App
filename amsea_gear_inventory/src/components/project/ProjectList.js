0ab9475215d9f09ff8d636daafb92abae23aae94
orthogonaltrajectory@gmail.com <orthogonaltrajectory@gmail.com>
2018-12-22 20:26:03 -0900

    Inventory item summaries display linked with redux store

--- amsea_gear_inventory/src/components/project/ProjectList.js
+++ amsea_gear_inventory/src/components/project/ProjectList.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ProjectSummary from './ProjectSummary'
 
-const ProjectList = () => {
+const ProjectList = ({projects}) => {
     return (
         <div className="project-list section">
-            <ProjectSummary />
-            <ProjectSummary />
-            <ProjectSummary />
-            <ProjectSummary />
+            {/* If projects exist, do this */}
+            { projects && projects.map(project => {
+                return (
+                    <ProjectSummary project={project} key={project.id} />
+                )
+            })}
         </div>
     )
 }
--- amsea_gear_inventory/src/store/reducers/projectReducer.js
+++ amsea_gear_inventory/src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+    projects: [
+        {id: '1', title: 'Tristan', content: 'stuff'},
+        {id: '2', title: 'Grubsy', content: 'more stuff'},
+        {id: '3', title: 'Grubby', content: 'even more stuff'}
+    ]
+}
 
 const projectReducer = (state = initState, action) => {
     return state
